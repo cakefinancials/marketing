5f811315cfc22edbb56caf9df84d3678851e5472
Samuel Perez <samuelp@socialnative.com>
2018-11-02 12:04:15 -0700

    finish up page 1

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react';
-import { Button, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
 import { ESPPDetailsCollector } from '../espp_profits/espp_details_collector';
 import { ESPPProfitsDisplay } from '../espp_profits/espp_profits_display';
+import { MethodologyExpander } from '../espp_profits/methodology_expander';
 
 import './Home.css';
 
@@ -10,6 +11,19 @@ export default class Home extends Component {
     super(props);
 
     this.state = { doneCollectingData: false };
+  }
+
+  renderESPPDetailsCollectorPageFooter() {
+    return (
+      <Fragment>
+        <p className='what-is-cake-fin'>What is Cake Financials?</p>
+        <p>
+          What is Cake Financials? Cake gives you interest-free money to invest in your Employee Stock Purchase Plan and
+          manages your account to maximize annual returns. Cake is completely free. If profits are made, we split them.
+          It is about time that you have your cake and eat it too!
+        </p>
+      </Fragment>
+    );
   }
 
   render() {
@@ -40,7 +54,18 @@ export default class Home extends Component {
           ) : (
             <ESPPDetailsCollector doneCollectingData={() => this.setState({ doneCollectingData: true })} />
           )}
+          <MethodologyExpander />
         </div>
+        <Row className='espp-calculator-footer' type='flex' justify='center'>
+          <Col span={10}>
+            <Row type='flex' justify='center'>
+              <Col className='espp-standalone-logo-container' />
+            </Row>
+            <Row type='flex' justify='center'>
+              {this.state.doneCollectingData ? null : this.renderESPPDetailsCollectorPageFooter()}
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
